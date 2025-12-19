2e3104baee340df0796e4cc631c4f5d8c54b0274
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2025-12-19 19:24:13 +0500

    Added model and optimizer saving functionality

--- VAE-GAN/train.py
+++ VAE-GAN/train.py
@@ -6,6 +6,15 @@ from engine import train
 from download_data import download_data
 from data_setup import create_dataloaders
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+LR = 3e-4
+EPOCHS = 25
+GAMMA = 20
+ALPHA = 0.1
+SAMPLE_SIZE = 0.5
+BATCH_SIZE = 64
+checkpoint_name = f"VAE-GAN-{EPOCHS}epoch-{SAMPLE_SIZE}sample_{LR}lr.pth"
+
 data_path = download_data(dataset_name= "Celeba")
 
 img_transforms = v2.Compose([
@@ -20,17 +29,12 @@ img_transforms = v2.Compose([
 
 (train_dataloader, test_dataloader, val_dataloader), (train_dataset, test_dataset, val_dataset) = create_dataloaders(data_path= data_path,
                                                                                                                  img_transforms= img_transforms,
-                                                                                                                sample_size = 0.5,
-                                                                                                                 batch_size= 64,
+                                                                                                                sample_size = SAMPLE_SIZE,
+                                                                                                                 batch_size= BATCH_SIZE,
                                                                                                                  num_workers= 4)
 
 print(len(train_dataloader), len(test_dataloader), len(val_dataloader), len(train_dataset), len(test_dataset), len(val_dataset))
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-LR = 3e-4
-EPOCHS = 25
-GAMMA = 20
-ALPHA = 0.1
 
 model = VAEGAN(in_channels=3,
                latent_dim=128,
@@ -48,10 +52,17 @@ train_results, test_results = train(model = model,
                                     test_dataloader= test_dataloader,
                                     discriminator_optimizer= discriminator_optimizer,
                                     decoder_optimizer= decoder_optimizer,
-                                    encoder_optimizer= encoder_optimizer
+                                    encoder_optimizer= encoder_optimizer,
                                     epochs = EPOCHS,
                                     gamma = GAMMA,
                                     device = device)
 
 plot_losses(results= train_results)
 plot_losses(results= test_results)
+
+save_checkpoint(model= model,
+                discriminator= model.discriminator,
+                discriminator_optimizer= discriminator_optimizer,
+                decoder_optimizer= decoder_optimizer,
+                encoder_optimizer= encoder_optimizer,
+                checkpoint_name= checkpoint_name)
