1806e22e7ef229c1803197d8a5d1d1aacf40c593
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2025-12-22 01:01:26 +0500

    Fixed plot_metrics and made saving optimizer and scheduler optional

--- RCAN/utils.py
+++ RCAN/utils.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import random
 
 def save_checkpoint(model: torch.nn.Module,
-                    optimizer: torch.optim.Optimizer,
-                    scheduler: torch.optim.lr_scheduler.LRScheduler,
-                    checkpoint_name: str):
+                    checkpoint_name: str,
+                    optimizer: torch.optim.Optimizer = None,
+                    scheduler: torch.optim.lr_scheduler.LRScheduler = None):
 
     assert checkpoint_name.endswith(".pth") or checkpoint_name.endswith(".pt"), "Checkpoint name should end with '.pt' or '.pth'"
     
@@ -22,15 +22,17 @@ def save_checkpoint(model: torch.nn.Module,
     scheduler_path.mkdir(parents=True, exist_ok=True)
 
     torch.save(obj = model.state_dict(), f= model_path / checkpoint_name)
-    torch.save(obj = optimizer.state_dict(), f= optimizer_path / checkpoint_name)
-    torch.save(obj = scheduler.state_dict(), f= scheduler_path / checkpoint_name)
+    if optimizer:
+        torch.save(obj = optimizer.state_dict(), f= optimizer_path / checkpoint_name)
+    if scheduler:
+        torch.save(obj = scheduler.state_dict(), f= scheduler_path / checkpoint_name)
 
     print("Model, optimizer, and scheduler are saved")
 
 def load_checkpoint(model: torch.nn.Module,
-                    optimizer: torch.optim.Optimizer,
-                    scheduler: torch.optim.lr_scheduler.LRScheduler,
                     checkpoint_name: str,
+                    optimizer: torch.optim.Optimizer = None,
+                    scheduler: torch.optim.lr_scheduler.LRScheduler = None,
                     devce: torch.device = "cuda"):
 
     assert checkpoint_name.endswith(".pt") or checkpoint_name.endswith(".pth"), "Checkpoint name should end with '.pt' or '.pth'"
@@ -40,8 +42,10 @@ def load_checkpoint(model: torch.nn.Module,
     scheduler_path = Path("schedulers")
     
     model.load_state_dict(torch.load(model_path / checkpoint_name))
-    optimizer.load_state_dict(torch.load(optimizer_path / checkpoint_name))
-    scheduler.load_state_dict(torch.load(scheduler_path / checkpoint_name))
+    if optimizer:
+        optimizer.load_state_dict(torch.load(optimizer_path / checkpoint_name))
+    if scheduler:
+        scheduler.load_state_dict(torch.load(scheduler_path / checkpoint_name))
 
 def evaluate_model(model: torch.nn.Module,
                    loss_fn: torch.nn.Module,
@@ -72,8 +76,8 @@ def evaluate_model(model: torch.nn.Module,
 
 def plot_metrics(results: Dict[str, List[float]]):
 
-    epochs = range(len(results))
-    plt.figure(figsize= (20, 16))
+    epochs = range(len(results["train_loss"]))
+    plt.figure(figsize= (12, 30))
     for i, (name, metric) in enumerate(results.items()):
         plt.subplot(len(results), 1, i+1)
         plt.plot(epochs, metric)
@@ -117,13 +121,13 @@ def plot_reconstructed_images(model: torch.nn.Module,
             i += 1
             plt.subplot(samples, 2, i)
             plt.imshow(y.permute(1, 2, 0))
-            plt.title("Original")
+            plt.title(f"Original, Size: {y.shape}")
             plt.axis(False)
     
             i += 1
             plt.subplot(samples, 2, i)
             plt.imshow(y_pred.permute(1, 2, 0))
-            plt.title(f"PSNR: {psnr:.5f} | SSIM: {ssim:.5f}")
+            plt.title(f"Reconstructed, PSNR: {psnr:.5f} | SSIM: {ssim:.5f}")
             plt.axis(False)
 
     
