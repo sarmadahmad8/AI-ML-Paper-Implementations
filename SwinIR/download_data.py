e6c7251178f4202c2151e527b53b48a96d1bd9fc
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2025-12-24 22:48:10 +0500

    Added download functionality for 'Manga109' dataset

--- SwinIR/download_data.py
+++ SwinIR/download_data.py
@@ -27,7 +27,7 @@ def download_data(dataset_name: str):
             print(f"Moved data to working directory!")
             shutil.rmtree(download_path)
 
-    else:
+    elif dataset_name == "Urban100":
         data_path = Path("../data/Urban100")
         if not data_path.is_dir():
             print(f"Dataset path: {data_path} does not exist, creating one ...")
@@ -43,6 +43,29 @@ def download_data(dataset_name: str):
         else:
             print(f"Data does not exist inside {data_path} , downloading data...")
             download_path = kagglehub.dataset_download("harshraone/urban100")
+            print(f"Moving data to working directory")
+            shutil.copytree(download_path,
+                            data_path,
+                            dirs_exist_ok=True)
+            print(f"Moved data to working directory!")
+            shutil.rmtree(download_path)
+
+    else:
+        data_path = Path("../data/Manga109")
+        if not data_path.is_dir():
+            print(f"Dataset path: {data_path} does not exist, creating one ...")
+            data_path.mkdir(parents=True,
+                            exist_ok=True)
+    
+        else:
+            print(f"Dataset path: {data_path} already exists, skipping creation ...")
+    
+        if any(item for item in data_path.iterdir()):
+            print(f"Data already exists inside: {data_path}, skipping download ...")
+    
+        else:
+            print(f"Data does not exist inside {data_path} , downloading data...")
+            download_path = kagglehub.dataset_download("guansuo/manga109")
             print(f"Moving data to working directory")
             shutil.copytree(download_path,
                             data_path,
