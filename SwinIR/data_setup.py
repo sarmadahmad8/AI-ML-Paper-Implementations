9209d937c54ad6fd1c0d8cff518c0c30d8e82ea9
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2025-12-24 22:47:33 +0500

    Added patch size customization and dataloader for 'Manga109' dataset

--- SwinIR/data_setup.py
+++ SwinIR/data_setup.py
@@ -13,13 +13,15 @@ class Div2K(Dataset):
                  image_dir: str,
                  scale: int,
                  sample_size: float,
+                 patch_size_lr: int = 64,
                  crops_per_image: int = 5):
 
         self.img_list = sorted(list(image_dir.glob("*/*.png")))
         self.sample_size = int(sample_size * len(self.img_list))
         self.img_list = self.img_list[:self.sample_size]
         self.scale = scale
-        self.patch_size_hr = scale * 48
+        self.patch_size_lr = patch_size_lr
+        self.patch_size_hr = scale * patch_size_lr
         self.crops_per_image = crops_per_image
         
         assert self.img_list != 0, "image directory has no images, check directory"
@@ -36,7 +38,7 @@ class Div2K(Dataset):
                    hr_img: Image.Image):
 
         width, height = hr_img.size
-        patch_size_lr = 48
+        patch_size_lr = self.patch_size_lr
 
         angles = [0, 90, 180, 270]
         angle = random.choice(angles)
@@ -88,18 +90,21 @@ def create_dataloaders_DIV2K(img_dir: str,
                            sample_size: float,
                            batch_size: int,
                            crops_per_image: int = 5,
+                           patch_size_lr: int = 64,
                            test_val_split: float = 0.5,
                            num_workers: int = 4):
 
     train_dataset = Div2K(image_dir= img_dir / "DIV2K_train_HR",
                           scale= scale,
                           sample_size= sample_size,
-                          crops_per_image= crops_per_image)
+                          crops_per_image= crops_per_image,
+                          patch_size_lr = patch_size_lr)
 
     test_dataset = Div2K(image_dir= img_dir / "DIV2K_valid_HR",
                          scale= scale,
                          sample_size= sample_size,
-                         crops_per_image= crops_per_image)
+                         crops_per_image= crops_per_image,
+                         patch_size_lr = patch_size_lr)
 
     test_split = int(test_val_split * len(test_dataset))
     val_split = len(test_dataset) - test_split
@@ -130,6 +135,7 @@ class Urban100(Dataset):
                  img_dir: str,
                  crops_per_image: int,
                  scale: int,
+                 patch_size_lr: int,
                  sample_size: float):
 
         self.img_dir = img_dir / "Urban 100" / f"X{scale} Urban100" / f"X{scale}"
@@ -140,7 +146,8 @@ class Urban100(Dataset):
         self.lr_img_list = self.lr_img_list[: self.sample_size]
         self.scale = scale
         self.crops_per_image = crops_per_image
-        self.patch_size_hr = scale * 48
+        self.patch_size_lr = patch_size_lr
+        self.patch_size_hr = scale * patch_size_lr
 
         assert len(self.hr_img_list) != 0 or len(self.lr_img_list) != 0, "Image directory does not have any images, check directory."
 
@@ -220,13 +227,15 @@ def create_dataloaders_Urban100(img_dir: str,
                                 scale: int,
                                 sample_size: float,
                                 batch_size: int,
+                                patch_size_lr: int = 64,
                                 train_test_val_split: List[float] = None,
                                 num_workers: int = 4):
 
     dataset = Urban100(img_dir= img_dir,
                        scale= scale,
                        crops_per_image= crops_per_image,
-                       sample_size= sample_size)
+                       sample_size= sample_size,
+                       patch_size_lr = patch_size_lr)
 
     if train_test_val_split:
         
@@ -262,3 +271,103 @@ def create_dataloaders_Urban100(img_dir: str,
                                     shuffle= False)
         
         return val_dataloader, dataset
+
+class Manga109(Dataset):
+    def __init__(self,
+                 image_dir: str,
+                 scale: int,
+                 sample_size: float,
+                 patch_size_lr: int = 64,
+                 crops_per_image: int = 5):
+
+        self.img_list = sorted(list(image_dir.glob("*/*.png")))
+        self.sample_size = int(sample_size * len(self.img_list))
+        self.img_list = self.img_list[:self.sample_size]
+        self.scale = scale
+        self.patch_size_lr = patch_size_lr
+        self.patch_size_hr = scale * patch_size_lr
+        self.crops_per_image = crops_per_image
+        
+        assert self.img_list != 0, "image directory has no images, check directory"
+
+    def load_image(self,
+                   index):
+
+        img_index = index // self.crops_per_image
+
+        hr_img = Image.open(self.img_list[index]).convert("RGB")
+        return hr_img
+
+    def _transforms(self,
+                   hr_img: Image.Image):
+
+        width, height = hr_img.size
+        patch_size_lr = self.patch_size_lr
+
+        angles = [0, 90, 180, 270]
+        angle = random.choice(angles)
+
+        if angle != 0:
+            hr_img = F.rotate(hr_img,
+                              angle= angle)
+
+        if random.random() > 0.5:
+            hr_img = F.hflip(hr_img)
+
+        left = random.randint(0, width - self.patch_size_hr)
+        top = random.randint(0, height - self.patch_size_hr)
+
+        hr_img = F.crop(img= hr_img,
+                       top= top,
+                       left= left,
+                       height= self.patch_size_hr,
+                       width= self.patch_size_hr)
+
+        lr_img = F.resize(img= hr_img,
+                          size=(patch_size_lr, patch_size_lr),
+                          interpolation= v2.InterpolationMode.BICUBIC)
+
+        lr_img, hr_img = F.to_tensor(pic= lr_img), F.to_tensor(pic= hr_img)
+
+        
+
+        return lr_img, hr_img
+        
+
+    def __len__(self):
+        
+        return len(self.img_list) * self.crops_per_image
+
+    def __getitem__(self,
+                    index: int):
+        
+        img_index = index // self.crops_per_image
+
+        hr_img = self.load_image(index= img_index)
+
+        lr_img, hr_img = self._transforms(hr_img)
+
+        return lr_img, hr_img
+
+def create_dataloaders_Manga109(img_dir: str,
+                               scale: int,
+                               sample_size: float,
+                               batch_size: int,
+                               crops_per_image: int = 5,
+                               test_val_split: float = 0.5,
+                               patch_size_lr: int = 64,
+                               num_workers: int = 4):
+
+    val_dataset = Div2K(image_dir= img_dir,
+                          scale= scale,
+                          sample_size= sample_size,
+                          crops_per_image= crops_per_image,
+                          patch_size_lr = patch_size_lr)
+
+
+    val_dataloader = DataLoader(dataset= val_dataset,
+                                  batch_size= batch_size,
+                                  num_workers= num_workers,
+                                  shuffle= False)
+
+    return val_dataloader, val_dataset
