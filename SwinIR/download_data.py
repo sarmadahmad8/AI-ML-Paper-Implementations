33d1ec49f3b83e8caeea144edf981a06d7dec4a1
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2025-12-25 19:10:37 +0500

    Added download functionality for'Kodak24' dataset

--- SwinIR/download_data.py
+++ SwinIR/download_data.py
@@ -50,7 +50,7 @@ def download_data(dataset_name: str):
             print(f"Moved data to working directory!")
             shutil.rmtree(download_path)
 
-    else:
+    elif dataset_name == "Manga109":
         data_path = Path("../data/Manga109")
         if not data_path.is_dir():
             print(f"Dataset path: {data_path} does not exist, creating one ...")
@@ -73,6 +73,28 @@ def download_data(dataset_name: str):
             print(f"Moved data to working directory!")
             shutil.rmtree(download_path)
 
+    else:
+        data_path = Path("../data/Kodak24")
+        if not data_path.is_dir():
+            print(f"Dataset path: {data_path} does not exist, creating one ...")
+            data_path.mkdir(parents=True,
+                            exist_ok=True)
+    
+        else:
+            print(f"Dataset path: {data_path} already exists, skipping creation ...")
+    
+        if any(item for item in data_path.iterdir()):
+            print(f"Data already exists inside: {data_path}, skipping download ...")
+    
+        else:
+            print(f"Data does not exist inside {data_path} , downloading data...")
+            download_path = kagglehub.dataset_download("drxinchengzhu/kodak24")
+            print(f"Moving data to working directory")
+            shutil.copytree(download_path,
+                            data_path,
+                            dirs_exist_ok=True)
+            print(f"Moved data to working directory!")
+            shutil.rmtree(download_path)
     return data_path
 
 if __name__ == "__main__":
