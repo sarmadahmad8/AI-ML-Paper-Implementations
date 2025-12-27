30607ffe7b4cd63297108569eb5e3950cff4ec98
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2025-12-27 12:36:25 +0500

    deleted train.py and renamed it to engine.py

--- Restormer/engine.py
+++ Restormer/engine.py
@@ -1,81 +1,32 @@
 from torchmetrics.functional import peak_signal_noise_ratio, structural_similarity_index_measure
 from tqdm.auto import tqdm
 import torch
+from torch.amp import autocast, GradScaler
 from kornia.color.ycbcr import RgbToYcbcr
 
 ycbcr = RgbToYcbcr()
-def train_step(model: torch.nn.Module,
-               loss_fn: torch.nn.Module,
-               optimizer: torch.optim.Optimizer,
-               scheduler: torch.optim.lr_scheduler,
-               train_dataloader: torch.utils.data.DataLoader,
-               device: torch.device,
-               discriminator: torch.nn.Module = None,
-               disc_optimizer: torch.optim.Optimizer = None,
-               disc_scheduler: torch.optim.lr_scheduler = None):
-
-    train_loss, train_psnr, train_ssim = 0.0, 0.0, 0.0
-
-    model.train()
-    for batch, (X, y) in tqdm(enumerate(train_dataloader)):
-        
-        X, y = X.to(device), y.to(device)
-        y_preds = model(X)
-        if not discriminator:
-            loss = loss_fn(y_preds, y)
-        else:
-            loss = loss_fn(preds = y_preds,
-                           targets = y,
-                           discriminator = discriminator)
-        
-        y_preds = ycbcr(y_preds)
-        y = ycbcr(y)
-        psnr = peak_signal_noise_ratio(y_preds[:, :1, :, :], y[:, :1, :, :])
-        ssim = structural_similarity_index_measure(y_preds[:, :1, :, :], y[:, :1, :, :])
-        
-        train_loss += loss.item()
-        train_psnr += psnr.item()
-        train_ssim += ssim.item()
-        
-        optimizer.zero_grad()
-        if discriminator:
-            disc_optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if discriminator:
-            disc_optimizer.step()
-        scheduler.step()
-        if discriminator:
-            disc_scheduler.step()
-
-    train_loss /= len(train_dataloader)
-    train_psnr /= len(train_dataloader)
-    train_ssim /= len(train_dataloader)
-
-    print(f" Train Loss: {train_loss:.5f} | Train PSNR: {train_psnr:.5f} | Train SSIM: {train_ssim:.5f}")
-
-    return train_loss, train_psnr, train_ssim
 
 def test_step(model: torch.nn.Module,
                loss_fn: torch.nn.Module,
                test_dataloader: torch.utils.data.DataLoader,
                device: torch.device,
-               discriminator: torch.nn.Module = None):
-
+               amp: bool = False):
+        
     test_loss, test_psnr, test_ssim = 0.0, 0.0, 0.0
 
     model.eval()
-    with torch.no_grad():
+    with torch.inference_mode():
         for batch, (X, y) in tqdm(enumerate(test_dataloader)):
             
             X, y = X.to(device), y.to(device)
-            y_preds = model(X)
-            if not discriminator:
-                loss = loss_fn(y_preds, y)
+            if amp:
+                with autocast(dtype= torch.float16,
+                             device_type= device):
+                    y_preds = model(X)
+                    loss = loss_fn(y_preds, y)
             else:
-                loss = loss_fn(preds = y_preds,
-                               targets = y,
-                               discriminator = discriminator)
+                y_preds = model(X)
+                loss = loss_fn(y_preds, y)
 
             y_preds = ycbcr(y_preds)
             y = ycbcr(y)
@@ -91,7 +42,9 @@ def test_step(model: torch.nn.Module,
         test_ssim /= len(test_dataloader)
 
     print(f" Test Loss: {test_loss:.5f} | Test PSNR: {test_psnr:.5f} | Test SSIM: {test_ssim:.5f}")
-
+    
+    model.train()
+    
     return test_loss, test_psnr, test_ssim
 
 def train(model: torch.nn.Module,
@@ -101,11 +54,11 @@ def train(model: torch.nn.Module,
            train_dataloader: torch.utils.data.DataLoader,
            test_dataloader: torch.utils.data.DataLoader,
            device: torch.device,
-           epochs: int,
-           discriminator: torch.nn.Module = None,
-           disc_optimizer: torch.optim.Optimizer = None,
-           disc_scheduler: torch.optim.lr_scheduler = None):
+           amp: bool = False):
 
+    if amp:
+        scaler = GradScaler()
+        
     results = {"train_loss": [],
                "train_psnr": [],
                "train_ssim": [],
@@ -115,28 +68,72 @@ def train(model: torch.nn.Module,
 
     model.to(device)
     
-    for epoch in tqdm(range(epochs)):
-        train_loss, train_psnr, train_ssim = train_step(model= model,
-                                                     train_dataloader= train_dataloader,
-                                                     loss_fn= loss_fn,
-                                                     optimizer= optimizer,
-                                                     scheduler= scheduler,
-                                                     device= device,
-                                                     discriminator = discriminator,
-                                                     disc_optimizer= disc_optimizer,
-                                                     disc_scheduler = disc_scheduler)
-        results["train_loss"].append(train_loss)
-        results["train_psnr"].append(train_psnr)
-        results["train_ssim"].append(train_ssim)
+    train_loss, train_psnr, train_ssim = 0.0, 0.0, 0.0
 
-        test_loss, test_psnr, test_ssim = test_step(model= model,
-                                                    test_dataloader= test_dataloader,
-                                                    loss_fn= loss_fn,
-                                                    device= device,
-                                                    discriminator = discriminator)
+    model.train()
+    for batch, (X, y) in tqdm(enumerate(train_dataloader)):
+        
+        X, y = X.to(device), y.to(device)
+        if amp:
+            with autocast(dtype= torch.float16,
+                         device_type= device):
+                y_preds = model(X)
+                loss = loss_fn(y_preds, y)
+        else:
+            y_preds = model(X)
+            loss = loss_fn(y_preds, y)
+            
+        y_preds = ycbcr(y_preds)
+        y = ycbcr(y)
+        psnr = peak_signal_noise_ratio(y_preds[:, :1, :, :], y[:, :1, :, :])
+        ssim = structural_similarity_index_measure(y_preds[:, :1, :, :], y[:, :1, :, :])
+        
+        train_loss += loss.item()
+        train_psnr += psnr.item()
+        train_ssim += ssim.item()
+        
+        optimizer.zero_grad()
+        if amp:
+            scaler.scale(loss).backward()
+        else:
+            loss.backward()
+        torch.nn.utils.clip_grad_norm_(parameters= model.parameters(),
+                                       max_norm= 1.0)
+        if amp:
+            scaler.step(optimizer=optimizer)
+            scaler.update()
+        else:
+            optimizer.step()
+        scheduler.step()
 
-        results["test_loss"].append(test_loss)
-        results["test_psnr"].append(test_psnr)
-        results["test_ssim"].append(test_ssim)
+        if (batch+1) % 1000 == 0:
+            
+            train_loss /= 1000
+            train_psnr /= 1000
+            train_ssim /= 1000
+
+            results["train_loss"].append(train_loss)
+            results["train_psnr"].append(train_psnr)
+            results["train_ssim"].append(train_ssim)
+            
+            print(f" Train Loss: {train_loss:.5f} | Train PSNR: {train_psnr:.5f} | Train SSIM: {train_ssim:.5f}")
+
+            train_loss, train_psnr, train_ssim = 0.0, 0.0, 0.0
+            
+            test_loss, test_psnr, test_ssim = test_step(model= model,
+                                                        test_dataloader= test_dataloader,
+                                                        loss_fn= loss_fn,
+                                                        device= device,
+                                                        amp = amp)
+            
+            results["test_loss"].append(test_loss)
+            results["test_psnr"].append(test_psnr)
+            results["test_ssim"].append(test_ssim)
+
+        if (batch+1) % 5000 == 0:
+            save_checkpoint(model= model,
+                            optimizer= optimizer,
+                            scheduler= scheduler,
+                            checkpoint_name= f"Restormer-GoPro-{batch+1}iterations-L1-64.pth")
 
     return results
