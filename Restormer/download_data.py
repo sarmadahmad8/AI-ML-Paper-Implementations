35201aca202c8cff7dea28d18539d1b8f8239afe
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2025-12-29 04:14:47 +0500

    Added 'Div2K' dataset downloader

--- Restormer/download_data.py
+++ Restormer/download_data.py
@@ -4,7 +4,30 @@ import kagglehub
 
 def download_data(dataset_name: str):
 
-    if dataset_name == "GoPro":
+    if dataset_name == "Div2K":
+        data_path = Path("../data/Div2K")
+        if not data_path.is_dir():
+            print(f"Dataset path: {data_path} does not exist, creating one ...")
+            data_path.mkdir(parents=True,
+                            exist_ok=True)
+    
+        else:
+            print(f"Dataset path: {data_path} already exists, skipping creation ...")
+    
+        if any(item for item in data_path.iterdir()):
+            print(f"Data already exists inside: {data_path}, skipping download ...")
+    
+        else:
+            print(f"Data does not exist inside {data_path} , downloading data...")
+            download_path = kagglehub.dataset_download("soumikrakshit/div2k-high-resolution-images")
+            print(f"Moving data to working directory")
+            shutil.copytree(download_path,
+                            data_path,
+                            dirs_exist_ok=True)
+            print(f"Moved data to working directory!")
+            shutil.rmtree(download_path)
+            
+    elif dataset_name == "GoPro":
         data_path = Path("../data/GoPro")
         if not data_path.is_dir():
             print(f"Dataset path: {data_path} does not exist, creating one ...")
