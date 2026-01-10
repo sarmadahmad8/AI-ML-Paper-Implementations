Fixed flow scaling issue

PWC-Net/model.py
@@ -307,8 +307,10 @@ class PWCNet(nn.Module):
         return cost_vol
 
     def forward(self,
-                I_1: torch.Tensor,
-                I_2: torch.Tensor) -> torch.Tensor:
+                I_1_2: torch.Tensor) -> torch.Tensor:
+
+        I_1 = I_1_2[:, :3]
+        I_2 = I_1_2[:, 3:]
 
         c_1_6, c_1_5, c_1_4, c_1_3, c_1_2 = self.fpen(I_1)
         c_2_6, c_2_5, c_2_4, c_2_3, c_2_2 = self.fpen(I_2)
@@ -325,7 +327,7 @@ class PWCNet(nn.Module):
 
         flow_input_6 = torch.cat((c_1_6, c_v_2_6, init_flow_6), dim= 1)
 
-        flow_6 = self.ofe6(flow_input_6)
+        flow_6 = self.ofe6(flow_input_6) + init_flow_6
 
         init_flow_5 = F.interpolate(input=flow_6,
                                scale_factor= 2,
@@ -341,7 +343,7 @@ class PWCNet(nn.Module):
 
         flow_input_5 = torch.cat((c_1_5, c_v_2_5, init_flow_5), dim= 1)
 
-        flow_5 = self.ofe5(flow_input_5)
+        flow_5 = self.ofe5(flow_input_5) + init_flow_5
 
         init_flow_4 = F.interpolate(input=flow_5,
                                scale_factor= 2,
@@ -357,7 +359,7 @@ class PWCNet(nn.Module):
 
         flow_input_4 = torch.cat((c_1_4, c_v_2_4, init_flow_4), dim= 1)
 
-        flow_4 = self.ofe4(flow_input_4)
+        flow_4 = self.ofe4(flow_input_4) + init_flow_4
 
         init_flow_3 = F.interpolate(input=flow_4,
                                scale_factor= 2,
@@ -373,7 +375,7 @@ class PWCNet(nn.Module):
 
         flow_input_3 = torch.cat((c_1_3, c_v_2_3, init_flow_3), dim= 1)
 
-        flow_3 = self.ofe3(flow_input_3)
+        flow_3 = self.ofe3(flow_input_3) + init_flow_3
 
         init_flow_2 = F.interpolate(input=flow_3,
                                scale_factor= 2,
@@ -381,7 +383,7 @@ class PWCNet(nn.Module):
                                align_corners=True)
                 
         warp_2 = self.warp_image(img= c_2_2,
-                                 flow= init_flow_2 * 5)
+                                 flow= init_flow_2 * 5.0)
 
         c_v_2_2 = self.cost_volume(c2w= warp_2,
                                    c1= c_1_2,
@@ -389,10 +391,17 @@ class PWCNet(nn.Module):
 
         flow_input_2 = torch.cat((c_1_2, c_v_2_2, init_flow_2), dim= 1)
 
-        flow_2 = self.ofe2(flow_input_2)
+        flow_2 = self.ofe2(flow_input_2) + init_flow_2
 
         cn_input = torch.cat((c_1_2, flow_2), dim= 1)
 
         refined_flow_2 = self.cn(cn_input) + flow_2
 
+        # print(f"flow_6: min={flow_6.min():.3f}, max={flow_6.max():.3f}, mean={flow_6.mean():.3f}, std={flow_6.std():.3f}")
+        # print(f"flow_5: min={flow_5.min():.3f}, max={flow_5.max():.3f}, mean={flow_5.mean():.3f}, std={flow_5.std():.3f}")
+        # print(f"flow_4: min={flow_4.min():.3f}, max={flow_4.max():.3f}, mean={flow_4.mean():.3f}, std={flow_4.std():.3f}")
+        # print(f"flow_3: min={flow_3.min():.3f}, max={flow_3.max():.3f}, mean={flow_3.mean():.3f}, std={flow_3.std():.3f}")
+        # print(f"flow_2: min={flow_2.min():.3f}, max={flow_2.max():.3f}, mean={flow_2.mean():.3f}, std={flow_2.std():.3f}")
+        # print(f"refined_flow_2: min={refined_flow_2.min():.3f}, max={refined_flow_2.max():.3f}, mean={refined_flow_2.mean():.3f}, std={refined_flow_2.std():.3f}")
+        
         return refined_flow_2, flow_3, flow_4, flow_5, flow_6