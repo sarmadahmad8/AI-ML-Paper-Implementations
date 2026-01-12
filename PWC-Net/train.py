570dd11abb843dff1230ceaf524455f5e0a874bc
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2026-01-12 22:07:56 +0500

    Changed 'SPyNet' train script to fit PWC-Net

--- PWC-Net/train.py
+++ PWC-Net/train.py
@@ -1,45 +1,38 @@
 import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import StepLR
-from model import SPyNet, ConvNet
-from loss import EndPointErrorLoss
+from model import PWCNet
+from PWCNet import PWCDCNet_old
+from loss import PWCNetLoss
 from data_setup import create_dataloaders_Sintel
 from utils import load_checkpoint
         
-EPOCHS = 150
+EPOCHS = 10
 LR = 1e-4
-BATCH_SIZE = 6
-LAYERS = 6
+BATCH_SIZE = 4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 train_dataloader, test_dataloader, train_dataset, test_dataset = create_dataloaders_Sintel(batch_size= BATCH_SIZE,
                                                                                            num_workers= 8)
 
-spynet = SPyNet(layers=LAYERS).to(device)
-# convnet = ConvNet()
-loss_fn = EndPointErrorLoss().to(device)
+# pwc_net = PWCDCNet_old().to(device)
+pwc_net = PWCNet().to(device)
 
-for i, convnet_module in enumerate(spynet.spy_net):
-    if i >= (LAYERS - 1):
-        checkpoint_path = f"models/ConvNet-{i}-Sintel-150epochs-3experiment.pth"
-        convnet_module.load_state_dict(torch.load(checkpoint_path))
-    else:
-        checkpoint_path = f"models/ConvNet-{i+1}-Sintel-150epochs-3experiment.pth"
-        convnet_module.load_state_dict(torch.load(checkpoint_path))
-
-    if i != (len(spynet.spy_net) - 1):
-        for param in convnet_module.parameters():
-            param.requires_grad = False
+loss_fn = PWCNetLoss().to(device)
             
-optimizer = torch.optim.Adam(params= filter(lambda p: p.requires_grad, spynet.parameters()),
+optimizer = torch.optim.Adam(params= pwc_net.parameters(),
                              lr= LR,
                              betas= (0.9, 0.999))
 
+load_checkpoint(model= pwc_net,
+                optimizer= optimizer,
+                checkpoint_name= "PWCNet-Sintel-5epochs-Experiment3.pth")
+
 scheduler = StepLR(optimizer= optimizer,
-                   step_size= 61 * 174,
+                   step_size= 61 * 131,
                    gamma= 0.1)
 
-results = train_Sintel(model= spynet,
+results = train_Sintel(model= pwc_net,
                        train_dataloader= train_dataloader,
                        test_dataloader= test_dataloader,
                        loss_fn= loss_fn,
@@ -47,4 +40,5 @@ results = train_Sintel(model= spynet,
                        scheduler = scheduler,
                        resize= (448, 1024),
                        epochs=EPOCHS,
-                       device= device)
+                       device= device,
+                       use_scaler= True)
