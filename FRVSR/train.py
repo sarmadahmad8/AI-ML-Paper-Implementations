c3e8ee1b594c48b72e423b51a28cc174e1902e44
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2026-01-15 15:24:54 +0500

    Added model evaluation to train script

--- FRVSR/train.py
+++ FRVSR/train.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 from pathlib import Path
 from model import FRVSR, init_params
-from data_setup import create_dataloaders
+from data_setup import create_dataloaders_custom, create_dataloaders_vid4
 from engine import train
-from utils import load_checkpoint, save_checkpoint
+from utils import load_checkpoint, save_checkpoint, plot_reconstructed_images
 from loss import FRVSRLoss
 
 
@@ -14,12 +14,18 @@ BATCH_SIZE = 2
 SAMPLE_SIZE = 0.05
 LR = 1e-4
 PRETRAINED_EPOCHS = 10
+USE_AMP = True
 EPOCHS = 10
 
+if USE_AMP:
+    PRECISION = "fp16"
+else:
+    PRECISION = "fp32"
+
 img_dir = Path("../data/VSRDataset/video_1_frames/")
-train_dataloader, test_dataloader, train_dataset, test_dataset = create_dataloaders(img_dir=img_dir, 
-                                                                                    sample_size=SAMPLE_SIZE,
-                                                                                    batch_size=BATCH_SIZE)
+train_dataloader, test_dataloader, train_dataset, test_dataset = create_dataloaders_custom(img_dir=img_dir, 
+                                                                                           sample_size=SAMPLE_SIZE,
+                                                                                           batch_size=BATCH_SIZE)
 
 print(len(train_dataloader))
 
@@ -29,8 +35,6 @@ frvsr = FRVSR(res_in_channels=128,
                 residual_blocks=10,
                 scale_factor=4).apply(init_params).to(device)
 
-# swin_ir = torch.compile(model= swin_ir)
-
 loss_fn = FRVSRLoss().to(device)
 
 optimizer = torch.optim.Adam(params= frvsr.parameters(),
@@ -38,7 +42,7 @@ optimizer = torch.optim.Adam(params= frvsr.parameters(),
                              betas=(0.9, 0.999))
 load_checkpoint(model= frvsr,
                 optimizer= optimizer,
-                checkpoint_name= f"FRVSR-CustomDataset-{PRETRAINED_EPOCHS}epochs-fp32.pth")
+                checkpoint_name= f"FRVSR-CustomDataset-{PRETRAINED_EPOCHS}epochs-{PRECISION}-{SAMPLE_SIZE}samplesize.pth")
 
 results = train(model= frvsr,
                 train_dataloader= train_dataloader,
@@ -48,8 +52,21 @@ results = train(model= frvsr,
                 scheduler=None,
                 device= device,
                 epochs= EPOCHS,
+                pretrained_epochs=0,
                 use_amp= False)
 
-save_checkpoint(model= frvsr,
-                optimizer= optimizer,
-                checkpoint_name= f"FRVSR-CustomDataset-{EPOCHS + PRETRAINED_EPOCHS}epochs-fp32.pth")
+img_dir = Path("../data/Vid4/")
+val_dataloader, val_dataset = create_dataloaders_vid4(img_dir=img_dir,
+                                                      batch_size=4)
+
+evaluate_model(model= frvsr,
+               loss_fn=loss_fn,
+               val_dataloader=val_dataloader,
+               crop_border=4,
+               device=device)
+
+plot_reconstructed_images(model=frvsr,
+                          val_dataset=val_dataset,
+                          samples = 2,
+                          device= "cpu",
+                          save_name=f"FRVSR-Vid4-{EPOCHS + PRETRAINED_EPOCHS}epochs-{PRECISION}-{SAMPLE_SIZE}samplesize-Evaluation")
