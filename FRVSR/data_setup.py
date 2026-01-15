0a61f6f1e3385efc26a55c229d2c7330a53e5c98
sarmadahmad8 <sarmad.ahmed11@gmail.com>
2026-01-15 15:23:21 +0500

    Added dataloader for 'Vid4' dataset

--- FRVSR/data_setup.py
+++ FRVSR/data_setup.py
@@ -99,14 +99,14 @@ class CustomVSRDataset(Dataset):
 
         return lr_sequence, hr_sequence
 
-def create_dataloaders(img_dir: str,
-                       sample_size: float = 0.1,
-                       patch_size: int = 256,
-                       seq_length: int = 10,
-                       stride: int = 10,
-                       img_extension: str = "jpg",
-                       batch_size: int = 1,
-                       num_workers: int = 8):
+def create_dataloaders_custom(img_dir: str,
+                              sample_size: float = 0.1,
+                              patch_size: int = 256,
+                              seq_length: int = 10,
+                              stride: int = 10,
+                              img_extension: str = "jpg",
+                              batch_size: int = 1,
+                              num_workers: int = 8):
 
     dataset = CustomVSRDataset(img_dir=img_dir,
                                sample_size= sample_size,
@@ -132,3 +132,148 @@ def create_dataloaders(img_dir: str,
                                  shuffle=False)
 
     return train_dataloader, test_dataloader, train_dataset, test_dataset
+
+class Vid4Dataset(Dataset):
+
+    def __init__(self,
+                 img_dir: str,
+                 seq_length: int,
+                 stride: int,
+                 patch_size: int,
+                 img_extension: str = "png"):
+
+        self.seq_length = seq_length
+        self.stride = stride
+        self.patch_size = patch_size
+
+        self.video_frames = []
+        self.video_info = []
+        self.patches_per_frame = []
+        self.total_sample_sequences = 0
+        
+        for dir_ in img_dir.iterdir():
+            if dir_.is_dir():
+                frames = sorted(list(dir_.glob(f"*.{img_extension}")))
+
+            self.video_frames.append(frames)
+
+        for i, _ in enumerate(self.video_frames):
+            first_img = Image.open(self.video_frames[i][0]).convert(mode="RGB")
+            self.width, self.height = first_img.size
+            self.vertical_patches = self.height // self.patch_size
+            self.horizontal_patches = self.width // self.patch_size
+            self.patches_per_frame.append(self.vertical_patches * self.horizontal_patches)
+
+        for i, frames in enumerate(self.video_frames):
+            num_sequences = (len(frames) - self.seq_length) // self.stride + 1
+            video_samples = num_sequences * self.patches_per_frame[i]
+
+            self.video_info.append({
+                "start_idx": self.total_sample_sequences,
+                "num_sequences": num_sequences,
+                "num_frames": len(frames),
+                "samples": video_samples})
+                                   
+
+            self.total_sample_sequences += video_samples
+
+    def get_patch_coordinates(self,
+                               patch_index: int) -> Tuple[int, int, int, int]:
+
+        row = patch_index // self.horizontal_patches
+        col = patch_index % self.horizontal_patches
+
+        y_start = row * self.patch_size
+        y_end = y_start + self.patch_size
+        x_start = col * self.patch_size
+        x_end = x_start + self.patch_size
+
+        return y_start, y_end, x_start, x_end
+
+    def _get_lr_sequence(self,
+                         sequence: torch.Tensor) -> torch.Tensor:
+
+        gaussian_blur = v2.GaussianBlur(kernel_size=(5, 5),
+                                        sigma=(1.5, 1.5))
+        
+        hr_image_noise = gaussian_blur(sequence)
+        lr_sequence = hr_image_noise[:, :, ::4, ::4]
+
+        return lr_sequence
+
+    def _transforms(self,
+                    lr_sequence: torch.Tensor,
+                    hr_sequence: torch.Tensor):
+
+        transforms = v2.Compose([
+            v2.ToDtype(dtype=torch.float32,
+                       scale=True)
+        ])
+
+        return transforms(lr_sequence), transforms(hr_sequence)
+
+    def __len__(self):
+
+        return self.total_sample_sequences
+
+    def __getitem__(self,
+                    index: int):
+
+        video_idx = 0
+        local_index = index
+
+        for i, info in enumerate(self.video_info):
+            if index < info["start_idx"] + info["samples"]:
+                video_idx = i
+                local_index = index - info["start_idx"]
+                break
+
+        frames = self.video_frames[video_idx]
+
+        sequence_index = local_index // self.patches_per_frame[video_idx]
+        patch_index = local_index % self.patches_per_frame[video_idx]
+
+        start_frame = sequence_index * self.stride
+
+        y_start, y_end, x_start, x_end = self.get_patch_coordinates(patch_index= patch_index)
+
+        sequence = []
+        for t in range(self.seq_length):
+
+            frame_idx = start_frame + t
+            img = Image.open(frames[frame_idx]).convert(mode="RGB")
+            frame = v2.PILToTensor()(img)
+            # print(frame.shape, frame.min(), frame.max(), frame.dtype)
+            patch = frame[:, y_start: y_end, x_start: x_end]
+            # print(patch.shape, patch.min(), patch.max(), patch.dtype)
+            sequence.append(patch)
+
+        hr_sequence = torch.stack(sequence, dim=0)
+        # print(hr_sequence.min(), hr_sequence.max())
+        lr_sequence = self._get_lr_sequence(sequence= hr_sequence)
+
+        lr_sequence, hr_sequence = self._transforms(lr_sequence=lr_sequence,
+                                                    hr_sequence=hr_sequence)
+
+        return lr_sequence, hr_sequence
+
+def create_dataloaders_vid4(img_dir: str,
+                            patch_size: int = 256,
+                            seq_length: int = 10,
+                            stride: int = 10,
+                            img_extension: str = "png",
+                            batch_size: int = 1,
+                            num_workers: int = 8):
+
+    val_dataset = Vid4Dataset(img_dir=img_dir,
+                              patch_size= patch_size,
+                              seq_length=seq_length,
+                              stride=stride,
+                              img_extension=img_extension)
+
+    val_dataloader = DataLoader(dataset= val_dataset,
+                                batch_size=batch_size,
+                                num_workers= num_workers,
+                                shuffle= True)
+
+    return val_dataloader, val_dataset
